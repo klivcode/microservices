554d4dc80e01967d293fbc4c4b934d2a717f7297
Bhim Raj <infobrb04@gmail.com>
2026-01-02 14:49:21 +0545

    feat(quizApp): added get the questions for the Users from the Db QuestionsWrapper

--- src/main/java/com/springsec/monotholic_to_microservices_quiz_application/controller/QuizController.java
+++ src/main/java/com/springsec/monotholic_to_microservices_quiz_application/controller/QuizController.java
@@ -1,11 +1,14 @@
 package com.springsec.monotholic_to_microservices_quiz_application.controller;
 
+import com.springsec.monotholic_to_microservices_quiz_application.entities.QuestionWrapper;
 import com.springsec.monotholic_to_microservices_quiz_application.entities.Questions;
 import com.springsec.monotholic_to_microservices_quiz_application.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -22,4 +25,12 @@ public class QuizController {
     {
         return quizService.createQuiz(category,numQ,title);
     }
+
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id)
+    {
+        return quizService.getQuizQuestions(id);
+    }
+
 }
--- src/main/java/com/springsec/monotholic_to_microservices_quiz_application/service/QuizService.java
+++ src/main/java/com/springsec/monotholic_to_microservices_quiz_application/service/QuizService.java
@@ -2,6 +2,7 @@ package com.springsec.monotholic_to_microservices_quiz_application.service;
 
 import com.springsec.monotholic_to_microservices_quiz_application.dao.QuestionDao;
 import com.springsec.monotholic_to_microservices_quiz_application.dao.QuizDao;
+import com.springsec.monotholic_to_microservices_quiz_application.entities.QuestionWrapper;
 import com.springsec.monotholic_to_microservices_quiz_application.entities.Questions;
 import com.springsec.monotholic_to_microservices_quiz_application.entities.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
-
-
-
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -36,5 +37,16 @@ public class QuizService {
         quiz.setQuestions(questions);
         quizDao.save(quiz);
         return new ResponseEntity<>("Success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quizs = quizDao.findById(id);
+        List<Questions> questionsFromDb= quizs.get().getQuestions();
+        List<QuestionWrapper> questionsForUsers = new ArrayList<>();
+        for(Questions q : questionsFromDb){
+            QuestionWrapper qw= new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionsForUsers.add(qw);
+        }
+        return new ResponseEntity<>(questionsForUsers, HttpStatus.OK);
     }
 }
